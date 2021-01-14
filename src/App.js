ad35eb164bfb6c0bf3fb904dcc9f30eaccec4472
Heirin <eirinbjorknes@gmail.com>
2021-01-14 14:19:24 +0100

    artikkel generert fra array

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import './App.css';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Post from "./components/Post"
+import artikler from "./artikkelliste.js";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <div className="blog-wrapper">
-      <Post heading="Jeg lager en blogg" content="Dette er et innlegg om hvordan jeg lager en blogg. Denne teksten skal egentlig autogenereres fra en database. vi får se hvordan det går."/>
-      <Post heading="Organisering av innhold" content="Jeg tenker å generere innleggene med nyeste innlegg øverst."/>
-      <Post heading="Videreutvikling" content="Jeg tenker å lage innlogging for admin, sånn at jeg kan redigere innholdet fra websiden."/>
-      <Post heading="Layout på innlegg" content="Jeg vil også finne ut hvordan jeg kan tilpasse bilder og sånt."/>
+        {artikler.map(artikkel=>(<Post heading={artikkel.tittel} content={artikkel.tekst}/>))}
       </div>
       <Footer/>
     </div>
---
+++ src/artikkelliste.js
@@ -0,0 +1,29 @@
+const artikkelliste = [
+  {
+    id: 5,
+    tittel: "Praesent quam leo",
+    tekst: "Aliquam ut egestas nisl. Nunc lorem est, condimentum ac luctus ac, feugiat at felis. Vivamus congue purus eget tortor volutpat tincidunt. Curabitur eu fermentum felis. Donec cursus felis nec sem faucibus, et ullamcorper sem lacinia. Pellentesque id nisi ante. Nam erat ipsum, maximus hendrerit condimentum ut, volutpat nec augue."
+  },
+  {
+    id: 4,
+    tittel: "Aliquam ut egestas nisl",
+    tekst: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+  },
+  {
+    id: 3,
+    tittel: "lipsum.com",
+    tekst: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."
+  },
+  {
+    id: 2,
+    tittel: "Lorem Ipsum",
+    tekst: "For å designe elementer som skal vises på siden før innholdet er bestemt, er et vanlig knep å bruke utdrag fra Ciceros ''De finibus bonorum et malorum'' som fylltekst. Dette gjøres fordi de ferreste leser latin, så teksten ikke distraherer. Den teksten som man kan hente fra generatorer på nettet er vanligvis ikke korrekt, men endret til formålet."
+  },
+  {
+    id: 1,
+    tittel: "Jeg lager en blogg!",
+    tekst: "Dette er en øvelse i å lage en blogg ved hjelp av det jeg har lært i et Udemy-kurs i webutvikling. Kurset tok for seg blant annet React, noe jeg vil bruke i dette prosjektet."
+  }
+]
+
+export default artikkelliste;

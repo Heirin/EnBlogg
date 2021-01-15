fbba4455b122981a983610e76ad20d64f05ebb08
Heirin <eirinbjorknes@gmail.com>
2021-01-15 13:20:23 +0100

    litt styling i CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Footer(){
-  return (<footer className="app-footer"><p>Eirin Skaar Bjørknes</p></footer>);
+  return (
+    <footer className="app-footer">
+      <p>Eirin Skaar Bjørknes</p>
+    </footer>
+  );
 }
 
 export default Footer;
